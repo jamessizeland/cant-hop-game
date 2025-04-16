0e36edb3fef17ffd68fd280ab1ea73206a10a111
James Sizeland <jamessizeland@gmail.com>
2025-04-16 23:20:31 +0100

    add comments

--- src-tauri/src/state.rs
+++ src-tauri/src/state.rs
@@ -12,25 +12,48 @@ pub struct SettingsState {
 #[derive(Default, Debug, Clone, Copy, Serialize, Deserialize)]
 pub enum PlayerMode {
     #[default]
+    /// A human player
     Human,
+    /// A safe AI player
+    /// This player will play safe and not risk any columns
     Safe,
+    /// A normal AI player
+    /// This player will play normally and risk columns
+    /// This player will risk columns based on the current game state
     Normal,
+    /// A risky AI player
+    /// This player will play risky and risk columns
+    /// This player will risk columns based on the current game state
     Risky,
 }
 
 /// A player in the game
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Player {
+    /// The player's mode
+    /// This is used to determine how the player will play
+    /// The player can be a human, a safe AI, a normal AI, or a risky AI
     mode: PlayerMode,
+    /// The player's ID
     id: usize,
+    /// The player's name
+    /// This is used to display the player's name in the UI
     name: String,
+    /// The number of columns the player has won
+    /// This is used to determine if the player has won
+    won_cols: Vec<usize>,
 }
 
 #[derive(Default, Debug, Copy, Clone, Serialize, Deserialize)]
 pub struct Column {
+    /// The dice number of the column
     pub col: usize,
+    /// The height of the column, which is the number of hops in the column
     pub height: usize,
+    /// The current position of each player in the column
     pub hops: [usize; 4],
+    /// The number of hops the current player has risked in the column
+    /// This is relative to their current position in the column.
     pub risked: usize,
 }
 
@@ -41,6 +64,26 @@ pub struct GameState {
     pub settings: SettingsState,
     pub current_player: usize,
     pub columns: [Column; 11],
+    pub winner: Option<Player>,
+}
+
+impl GameState {
+    /// Set to next player
+    pub fn next_player(&mut self) {
+        self.current_player = (self.current_player + 1) % self.settings.players.len();
+    }
+
+    /// Check if the game is over
+    pub fn check_is_over(&mut self) {
+        for player in &self.settings.players {
+            if player.won_cols.len() >= self.settings.win_cols {
+                self.winner = Some(player.clone());
+            }
+        }
+        if let Some(winner) = self.winner.as_ref() {
+            println!("Game Over! Player {} wins!", winner.name);
+        }
+    }
 }
 
 impl Default for GameState {
@@ -52,17 +95,20 @@ impl Default for GameState {
                         mode: PlayerMode::Human,
                         id: 0,
                         name: "Player 1".to_string(),
+                        won_cols: vec![],
                     },
                     Player {
                         mode: PlayerMode::Normal,
                         id: 1,
                         name: "Player 2".to_string(),
+                        won_cols: vec![],
                     },
                 ],
                 win_cols: 3,
             },
             current_player: 0,
             columns: generate_columns(),
+            winner: None,
         }
     }
 }
