7174b7d399c08cf2b93db966201fd1b291887dc9
James Sizeland <jamessizeland@gmail.com>
2025-04-18 22:23:30 +0100

    speed up animations

--- src/components/elements/rolling.tsx
+++ src/components/elements/rolling.tsx
@@ -17,7 +17,7 @@ const diceVariants = {
     filter: "blur(0px)",
     transition: {
       delay: custom * 0.1, // Delay for staggered effect
-      duration: 0.8, // Longer duration for a more dramatic effect
+      duration: 0.4, // Longer duration for a more dramatic effect
       ease: "easeOut",
     },
   }),
@@ -29,7 +29,7 @@ const choicesVariants = {
     opacity: 1,
     y: 0,
     transition: {
-      delay: custom * 0.1 + 1, // Adds a delay for the choices (delay after dice animation)
+      delay: custom * 0.1 + 0.5, // Adds a delay for the choices (delay after dice animation)
       duration: 0.5,
     },
   }),
@@ -68,35 +68,39 @@ const DiceRoller = ({ setGameState }: RollerProps) => {
     }
   };
 
+  const endPlayerTurn = async (forced: boolean) => {
+    const state = await endTurn(forced);
+    setDice({ dice: [], choices: [] });
+    setHops([]);
+    setGameState(state);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center space-y-6 p-4">
       {!dice.dice.length ? (
         <div className="flex flex-row items-center justify-center space-x-6">
           <motion.button
+            className="btn btn-xl bg-green-400 text-black disabled:opacity-50"
+            type="button"
+            onClick={async () => await updateDice()}
+            // Animation options
             initial={{ scale: 0 }}
             animate={{ scale: 1 }}
             exit={{ scale: 0 }}
-            transition={{ duration: 0.5 }}
-            onClick={updateDice}
-            className="btn btn-xl bg-green-400 text-black disabled:opacity-50"
-            type="button"
+            transition={{ duration: 0.1 }}
           >
             {hops.length ? "Hop?" : "Hop!"}
           </motion.button>
           {hops.length > 0 ? (
             <motion.button
+              className="btn btn-xl bg-green-400 text-black disabled:opacity-50"
+              type="button"
+              onClick={async () => await endPlayerTurn(false)}
+              // Animation options
               initial={{ scale: 0 }}
               animate={{ scale: 1 }}
               exit={{ scale: 0 }}
-              transition={{ duration: 0.5 }}
-              onClick={async () => {
-                const state = await endTurn();
-                setDice({ dice: [], choices: [] });
-                setHops([]);
-                setGameState(state);
-              }}
-              className="btn btn-xl bg-green-400 text-black disabled:opacity-50"
-              type="button"
+              transition={{ duration: 0.1 }}
             >
               Stop?
             </motion.button>
@@ -112,6 +116,7 @@ const DiceRoller = ({ setGameState }: RollerProps) => {
           <motion.div
             key={index}
             className="w-16 h-16 bg-white rounded shadow flex items-center justify-center text-2xl font-bold border border-gray-200"
+            // Animation options
             custom={index}
             initial="hidden"
             animate="visible"
@@ -121,39 +126,33 @@ const DiceRoller = ({ setGameState }: RollerProps) => {
           </motion.div>
         ))}
       </div>
-      <div className="grid grid-flow-col-dense space-x-4">
+      <div className="flex flex-row flex-wrap space-x-4 space-y-4 justify-center">
         {dice.choices.length ? (
           dice.choices.map((choice, index) => (
             <motion.button
               key={index}
-              className="btn btn-outline btn-primary btn-lg shadow flex items-center justify-center text-2xl font-bold w-max"
+              className="btn btn-outline btn-primary btn-lg shadow justify-center text-2xl font-bold h-16 min-w-16"
+              type="button"
+              onClick={async () => await makeChoice(choice)}
+              // Animation options
               custom={index}
               initial="hidden"
               animate="visible"
               variants={choicesVariants}
-              type="button"
-              onClick={() => makeChoice(choice)}
             >
-              {choice[0]} {choice[1] ? `, ${choice[1]}` : ""}
+              {choice[0]} {choice[1] ? `& ${choice[1]}` : ""}
             </motion.button>
           ))
         ) : dice.dice.length ? (
           <motion.button
-            className="btn btn-outline btn-primary btn-lg shadow flex items-center justify-center text-2xl font-bold w-max"
+            className="btn btn-outline btn-primary btn-lg justify-center text-2xl font-bold w-max"
+            type="button"
+            onClick={async () => await endPlayerTurn(true)}
+            // Animation options
             custom={0}
             initial="hidden"
             animate="visible"
             variants={choicesVariants}
-            type="button"
-            onClick={async () => {
-              const state = await endTurn();
-              setDice({ dice: [], choices: [] });
-              if (state) {
-                setGameState(state);
-              } else {
-                notifyError("Something went wrong ending turn", "endTurnError");
-              }
-            }}
           >
             Croaked!
           </motion.button>
