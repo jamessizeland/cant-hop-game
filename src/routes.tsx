c4000d68791aaf2a7b70f581c76c6edfb218c7d1
James Sizeland <jamessizeland@gmail.com>
2025-04-18 21:37:42 +0100

    add animated page transitions

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { SplashPage, GamePage, StatsPage, SettingsPage } from "pages";
 import { checkEnv } from "utils";
 import { MdDataExploration, MdGames, MdSettings } from "react-icons/md";
+import { AnimatePresence, motion } from "motion/react";
 
 type RouteType = {
   title: string;
@@ -47,11 +48,34 @@ export const routes: RouteType[] = checkEnv("development")
   : allRoutes;
 
 export default function AppRoutes(): React.ReactNode {
+  const location = useLocation();
   return (
-    <Routes>
-      {routes.map(({ path, element, title }) => (
-        <Route key={title} path={path} element={element} />
-      ))}
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        {/* Map through the routes and create a Route for each */}
+        {routes.map(({ path, element, title }) => (
+          // Wrap each route in an AnimationWrapper for animation effects
+
+          <Route
+            key={title}
+            path={path}
+            element={<AnimationWrapper>{element}</AnimationWrapper>}
+          />
+        ))}
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+function AnimationWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
+    >
+      {children}
+    </motion.div>
   );
 }
