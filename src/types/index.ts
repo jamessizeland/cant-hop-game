24602555385112e8441c0489471fca698b4d45b3
James Sizeland <jamessizeland@gmail.com>
2025-04-18 23:20:24 +0100

    add data for unavailable columns

--- src/types/index.ts
+++ src/types/index.ts
@@ -21,6 +21,8 @@ export type Column = {
   hops: [number, number, number, number];
   /** Number of hops risked beyond safe.  Must be +ve int */
   risked: number;
+  /** Whether the column is locked */
+  locked: boolean;
 };
 
 /** 11 columns, one for each possible number on 2D6 */
